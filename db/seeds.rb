e530c151385c1263e2f70c89a01787f212c9d08c
github username <github email address>
2018-03-27 08:53:13 -0400

    things are all working..journal entry rendered on different components..time to work on auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@
 
 User.create(name: "Andrea", birthday: 19930319, password: 'password')
 
-Mood.create(mood: 'on the verge of tears', url: "https://i.imgur.com/9jD56an.png" )
-Mood.create(mood: "I've had better", url: "https://i.imgur.com/2y5fxrA.png")
-Mood.create(mood: 'neutral', url: "https://i.imgur.com/YRsBKMi.png")
-Mood.create(mood: 'happy camper', url: "https://i.imgur.com/kEiCYh9.png")
-Mood.create(mood: 'on cloud nine', url: "https://i.imgur.com/gX2CRgx.png")
+Mood.create(mood: 'on the verge of tears', url: "https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/loudly-crying-face_1f62d.png" )
+Mood.create(mood: "I've had better", url: "https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/disappointed-face_1f61e.png")
+Mood.create(mood: 'neutral', url: "https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/neutral-face_1f610.png")
+Mood.create(mood: 'happy camper', url: "https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/grinning-face_1f600.png")
+Mood.create(mood: 'on cloud nine', url: "https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/upside-down-face_1f643.png")
 
 Activity.create(activity: 'work', url:"https://i.imgur.com/dCNBlhF.png ")
 Activity.create(activity: 'school', url:"https://i.imgur.com/Ak66hWQ.png")
