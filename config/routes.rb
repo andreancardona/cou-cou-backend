c98d2d765a6c76a7c41b0dd5b8bb98ed3654ef35
github username <github email address>
2018-03-28 17:25:05 -0400

    things are done...but logs need to be rendered immediately

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/logs/:log_id/activities', to: "activity_logs#index"
   post "/login", to: 'auth#login'
   post "/fetch_current_user", to: 'auth#decode'
+  get '/users/:id/logs', to: "logs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
